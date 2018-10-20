66e0a09d38dfc9cfd79e6c9a2ddaae318e45f566
Ernest Holloway <ernest.holloway@embersoftwarellc.com>
2018-10-20 01:15:58 -0500

    Fixed WellDataListItemVMTest

--- app/src/androidTest/java/qv21/codingexercise/integrationtests/WellDataListItemVMTest.java
+++ app/src/androidTest/java/qv21/codingexercise/integrationtests/WellDataListItemVMTest.java
@@ -1,19 +1,16 @@
 package qv21.codingexercise.integrationtests;
 
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.inject.Inject;
 
 import qv21.codingexercise.BaseAndroidUnitTest;
-import qv21.codingexercise.activities.MainActivity;
 import qv21.codingexercise.facades.WellDataFacade;
 import qv21.codingexercise.managers.MainActivityProviderManager;
 import qv21.codingexercise.managers.NavigationManager;
@@ -24,9 +21,6 @@ import qv21.codingexercise.views.WellDataDetailsScreen;
 
 @RunWith(AndroidJUnit4.class)
 public class WellDataListItemVMTest extends BaseAndroidUnitTest {
-    @Rule
-    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
     @Inject
     NavigationManager navigationManager;
 
@@ -44,8 +38,6 @@ public class WellDataListItemVMTest extends BaseAndroidUnitTest {
     @Before
     public void setup() {
         getTestAppComponent().inject(this);
-
-        wellDataListItemVM = new WellDataListItemVM(wellDataFacade, navigationManager, mainActivityProviderManager, screenManager);
     }
 
     @After
@@ -55,6 +47,8 @@ public class WellDataListItemVMTest extends BaseAndroidUnitTest {
 
     @Test
     public void navigateToWellDataDetailsScreenTest() {
+        wellDataListItemVM = new WellDataListItemVM(wellDataFacade, navigationManager, mainActivityProviderManager, screenManager);
+
         WellDataDM wellData = new WellDataDM();
 
         wellDataListItemVM.wellData.set(wellData);
